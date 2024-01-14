0abea3bc90707cc564d49ba4b66d6d3224bc1b12
Matt Hammond <mjhammond94@gmail.com>
2024-01-14 22:45:07 +0000

    Add extra tests for bonus round scenarios - tests pass

--- bowling_for_soup/lib/scorecard.rb
+++ bowling_for_soup/lib/scorecard.rb
@@ -1,6 +1,6 @@
 class Scorecard
 
-  attr_reader :current_frame, :total_score, :bonus_points, :strike_streak, :active_frame_score
+  attr_reader :current_frame, :total_score, :bonus_points, :strike_streak, :active_frame_score, :strike_round
 
   def initialize
     @current_frame = 1
@@ -15,27 +15,32 @@ class Scorecard
   def add_frame(roll, score)
     error_catcher(roll, score)
 
-    # Set a new game (reset all counts) if @current_frame >= 13 
-
-    new_game(roll)
-
-    if @current_frame == 11 && roll == 1 && score == 10
-      return strike
-    elsif @current_frame == 12 && roll == 1 && score == 10
-      @bonus_points += 10 
-      return strike
-    elsif @current_frame == 11 && @spare_round 
-      return @bonus_points += score
-    else 
-      "else2"
+    # How many scenarios?
+    # For frames greater than 10 ():
+    if @current_frame > 10
+      if @strike_round && @current_frame == 11 && score != 10
+        @total_score += score * 2
+        return @strike_round = false
+      elsif !@strike_round && @current_frame == 12 && roll == 2
+        return @total_score
+      elsif @current_frame == 11 && roll == 1 && score == 10
+        return strike
+      elsif @current_frame == 12 && roll == 1 && score == 10
+        @bonus_points += 10 
+        return strike
+  
+      elsif @current_frame == 11 && @spare_round 
+        return @bonus_points += score
+      else 
+        "else2"
+      end
     end
+
 
     if @spare_round && roll == 1
       @bonus_points += score
       @spare_round = false
-    end
-
-    if @strike_round && score == 10
+    elsif @strike_round && score == 10
       @strike_streak > 1 ? @bonus_points += 10 : nil
       @bonus_points += 10
       return strike
@@ -43,7 +48,8 @@ class Scorecard
       @bonus_points += score
       @strike_round = false
       @strike_streak = 0
-    elsif @strike_round
+    elsif @strike_round 
+      @strike_streak > 1 ? @bonus_points += score : nil
       @bonus_points += score
     end
 
@@ -66,16 +72,6 @@ class Scorecard
 
   private
 
-  def new_game(roll)
-    if @current_frame >= 14
-      reset_game
-    elsif @current_frame == 11 && !@strike_round && !@spare_round
-      reset_game
-    else
-      "else"
-    end
-  end
-
   def reset_game
     @current_frame = 1
     @total_score = 0
@@ -91,12 +87,14 @@ class Scorecard
   end
 
   def spare(score)
+    @strike_streak = 0
     @total_score += score 
     @active_frame_score = 0
     @spare_round = true
   end
 
   def standard_roll(score)
+    @strike_streak = 0
     @active_frame_score += score
     @total_score += score
   end
@@ -113,6 +111,10 @@ class Scorecard
       error = "Score cannot exceed 10 in one roll"
     elsif roll > 2
       error = "Roll cannot be greater than 2 on a standard frame."
+    elsif roll < 1
+      error = "Roll cannot be less than 1."
+    elsif score < 0
+      error = "Score cannot be less than 0 for a roll."
     else 
       error = ""
     end
