ae9e66f4091b46355cbc92a6a44fa309b5dec4aa
Matt Hammond <mjhammond94@gmail.com>
2024-01-16 21:32:55 +0000

    Refactor change calc solution

--- ruby/change_calculator/lib/change_calculator.rb
+++ ruby/change_calculator/lib/change_calculator.rb
@@ -14,44 +14,23 @@ class ChangeCalculator
 
   def coins_needed(amount:)
     return [] if amount.zero?
-    raise StandardError.new("Value exceeds available coins.") if amount > @current_denominations.map { |k, v| k * v }.sum
-    return [amount] if @current_denominations.keys.include?(amount) && @current_denominations[amount] > 0
+    raise StandardError.new("Value exceeds available coins.") if amount > available_amount
 
     required_coins = []
 
     @current_denominations.keys.each do |available_coin|
-
-      if amount >= available_coin 
-        iterations = (amount / available_coin)
-        if iterations > @current_denominations[available_coin]
-          amount -= available_coin * @current_denominations[available_coin]
-          @current_denominations[available_coin].times { required_coins << available_coin }
-          @current_denominations[available_coin] = 0
-        else 
-          amount -= available_coin * iterations
-          iterations.times { required_coins << available_coin }
-          @current_denominations[available_coin] -= iterations
-        end 
-      end
+      iterations = (amount / available_coin)
+      individual_availabile = iterations > @current_denominations[available_coin]
+      individual_availabile ? amount -= available_coin * @current_denominations[available_coin] : amount -= available_coin * iterations
+      individual_availabile ? @current_denominations[available_coin].times { required_coins << available_coin } : iterations.times { required_coins << available_coin }
+      individual_availabile ? @current_denominations[available_coin] = 0 : @current_denominations[available_coin] -= iterations
     end
     required_coins
   end
 
-  # private
+  private 
 
-  # DENOMINATIONS = [
-  #   200,
-  #   100,
-  #    50,
-  #    20,
-  #    10,
-  #     5,
-  #     2,
-  #     1,
-  # ].freeze
-  # private_constant :DENOMINATIONS
-
-  # def available_coins
-  #   DENOMINATIONS
-  # end
+  def available_amount 
+    @current_denominations.map { |k, v| k * v }.sum
+  end
 end
